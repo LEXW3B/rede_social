0f86d91bab962219353e298803b0be3c38871cce
Alexandre E.S.L <alexmanson12345@gmail.com>
2023-12-06 18:01:22 -0300

    feat: rota '/' para LoginForm

--- front/app/src/App.js
+++ front/app/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <LoginForm />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+      </Routes>
+    </Router>
   );
 }
 
